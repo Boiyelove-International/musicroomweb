329cde35d9e2c00c8f85c2113719244adafb8d84
boiyelove <daahrmmieboiye@gmail.com>
2021-12-10 15:20:05 +0100

    Added event app and absctract constructs

---
+++ events/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from model_utils.models import TimeStampedModel
+
+# Create your models here.
+class Event(TimeStampedModel):
+	pass
+
+class Playlist(TimeStampedModel):
+	pass
+
+class Song(TimeStampedModel):
+	pass
